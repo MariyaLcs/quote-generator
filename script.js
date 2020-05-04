47c51f0f752975844da8db28ee75c5c0c19f99f7
Mariya <mariya.podosinova@gmail.com>
2020-05-04 13:09:14 +0100

    XHR request

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-$("#btn").click(function () {
-  $.getJSON("https://aws.random.cat/meow")
-    .done(function (data) {
-      $("#photo").attr("src", data.file);
-    })
-    .fail(function () {
-      alert("REQUEST IS NOT PAWSIBBLE");
-    });
+let url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+let xhrbtn = document.querySelector("#xhr"),
+  fetchbtn = document.querySelector("#fetch"),
+  axiosbtn = document.querySelector("#axios"),
+  display = document.querySelector("#quote");
+
+xhrbtn.addEventListener("click", function () {
+  let XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function () {
+    if (XHR.readyState == 4 && XHR.status == 200) {
+      var quote = JSON.parse(XHR.responseText)[0];
+      display.innerText = quote;
+    }
+  };
+  XHR.open("GET", url);
+  XHR.send();
 });
